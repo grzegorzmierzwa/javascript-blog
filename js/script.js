2124a609a0288f939a42b13b3abedc4bb54e54d5
Grzegorz Mierzwa <mierzwagrzegorz@gmail.com>
2019-10-11 14:21:06 +0200

    Added functions which remove articles after click on links

--- js/script.js
+++ js/script.js
@@ -1,24 +1,36 @@
 'use strict';
 
-const titleClickHandler = function(event){
-    console.log('Link was clicked!');
-    console.log(event);
-  
-    /* remove class 'active' from all article links  */
-  
-    /* add class 'active' to the clicked link */
-  
-    /* remove class 'active' from all articles */
-  
-    /* get 'href' attribute from the clicked link */
-  
-    /* find the correct article using the selector (value of 'href' attribute) */
-  
-    /* add class 'active' to the correct article */
+const titleClickHandler = function (event) {
+  console.log('Link was clicked!');
+  console.log(event);
+
+  /* remove class 'active' from all article links  */
+
+  const activeLinks = document.querySelectorAll('.titles a.active');
+
+  for (let activeLink of activeLinks) {
+    activeLink.classList.remove('active');
   }
-  
-  const links = document.querySelectorAll('.titles a');
-  
-  for(let link of links){
-    link.addEventListener('click', titleClickHandler);
+
+  /* add class 'active' to the clicked link */
+
+  const activeArticles = document.querySelectorAll('.posts article.active');
+
+  for (let activeArticle of activeArticles) {
+    activeArticle.classList.remove('active');
   }
+
+  /* remove class 'active' from all articles */
+
+  /* get 'href' attribute from the clicked link */
+
+  /* find the correct article using the selector (value of 'href' attribute) */
+
+  /* add class 'active' to the correct article */
+}
+
+const links = document.querySelectorAll('.titles a');
+
+for (let link of links) {
+  link.addEventListener('click', titleClickHandler);
+}
